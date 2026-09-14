6cd28a7dea7cc45a6a17dc0e6243e4b7a8637bdc
Reven Vinzon <revenvinzon011@gmail.com>
2026-09-14 13:58:27 +0800

    commit

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Paste the deployed Google Apps Script web app URL here.
-export const API_URL = 'https://script.google.com/macros/s/AKfycbwUqPvhkgiMg918ap3LbHUkZho9PVubf0n6k9-lF8VbE3-Jhq7W0Wo633KKQyGuK66Bmw/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbz2pEc6aCqKnzXIdiPQZBH6lc6X9TUZewPmS2RfuuSTh9UKSERrakfcH13OrlsrCcH9Zw/exec';
